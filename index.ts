dbd05e233d9069721294dc4b234efe14538831e3
xiaolidan00 <764937567@qq.com>
2025-06-22 10:55:19 +0800

    fix: 优化首页

--- index.ts
+++ index.ts
@@ -5,30 +5,37 @@ nav.innerHTML = '';
 const app = document.getElementById('app') as HTMLIFrameElement;
 let str = '';
 let index = 0;
+const hash = window.location.hash.substring(1);
+
+let current = 0;
 for (const k in urls) {
-  str += `<div index="${index}" key="${k}">${k}</div>`;
+  str += `<div ><span index="${index}" key="${k}">${k}</span><img key="${k}"  src="link.svg"></div>`;
+  if (hash && hash == k) {
+    current = index;
+  }
   index++;
 }
 nav.innerHTML = str;
 
-const first = nav.firstElementChild as HTMLDivElement;
-setPath(first);
-
-index = 0;
-function setPath(el: HTMLElement) {
-  const key = el.getAttribute('key');
-  if (key) {
-    const beforeEl = nav.children[index];
-    if (beforeEl) beforeEl.classList.remove('active');
-
-    const path = urls[key as keyof typeof urls];
-    app.src = path.replace('.ts', '.html');
-
-    el.classList.add('active');
-    index = Number(el.getAttribute('index') || '0');
-  }
+if (hash) {
+  const el = nav.children[current];
+  app.src = `src/${hash}/index.html`;
+  el.classList.add('active');
+} else {
+  const first = nav.firstElementChild as HTMLDivElement;
+  first.classList.add('active');
 }
+
 nav.onclick = (ev: MouseEvent) => {
   const target = ev.target as HTMLElement;
-  setPath(target);
+  if (target.nodeName.toLowerCase() === 'span') {
+    const key = target.getAttribute('key');
+    console.log('🚀 ~ key:', key);
+    app.src = `src/${key}/index.html`;
+    window.location.hash = '#' + key;
+    target.classList.add('active');
+    current = Number(target.getAttribute('index') || '0');
+  } else if (target.nodeName.toLowerCase() === 'img') {
+    window.open(`src/${target.getAttribute('key')}/index.html`);
+  }
 };
